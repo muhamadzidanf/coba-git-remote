3c0b9ccd7731ba1f21865a5996cc3c8964d4e909
muhamadzidanf <93507407+muhamadzidanf@users.noreply.github.com>
2022-04-18 13:44:07 +0700

    menambah input pekerjaan
    input dan output pekerjaan

--- py_coba.py
+++ py_coba.py
@@ -2,6 +2,7 @@ print("Paragraf pertama")
 
 nama = input("Masukan Nama : ")
 umur = input("Masukan umur")
+pekerjaan = input("Masukan pekerjaan : ")
 
-print(f"Nama anda {nama} dengan usia {umur}")
+print(f"Nama anda {nama} dengan usia {umur} pekerjaan anda {pekerjaan}")
 
