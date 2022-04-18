056042327f29b9f4737a081a936f726d2ff27c62
muhamadzidanf <muhamadzidan681@gmail.com>
2022-04-18 13:46:03 +0700

    menambahkan inputan hoby

--- py_coba.py
+++ py_coba.py
@@ -2,6 +2,7 @@ print("Paragraf pertama")
 
 nama = input("Masukan Nama : ")
 umur = input("Masukan umur")
+hobi = input("Masukan hobi anda : ")
 
-print(f"Nama anda {nama} dengan usia {umur}")
+print(f"Nama anda {nama} dengan usia {umur} hobby anda {hobi}")
 
